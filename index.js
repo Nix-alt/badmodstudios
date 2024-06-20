fa544c3dddda94dcc0f09d5c05abeda67b99fd2b
Nix-alt <167569462+Nix-alt@users.noreply.github.com>
2024-06-20 14:43:38 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function clicked(id){
+    switch(id) {
+        case "nix": {
+            window.location.href = "./nix/index.html";
+            break;
+        }
+        case "chroma": {
+            window.location.href = "./index.css";
+            break;
+        }
+    }
+}
